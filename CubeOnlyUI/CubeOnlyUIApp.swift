bb2d39d4f6655bf212a8228d3cbd42119d58a3cc
LeeTaehyung <thlee@kb-sys.com>
2022-08-17 14:53:34 +0900

    Initial Commit

---
+++ CubeOnlyUI/CubeOnlyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeOnlyUIApp.swift
+//  CubeOnlyUI
+//
+//  Created by Taehyung Lee on 2022/08/17.
+//
+
+import SwiftUI
+
+@main
+struct CubeOnlyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
